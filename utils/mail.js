a4d2aa9cfff9aeb6b1fb620fe4fecb0f3a96b465
Rakesh <rakeshchandra.offcl@gmail.com>
2020-12-09 23:23:49 +0700

    small cchanges

--- utils/mail.js
+++ utils/mail.js
@@ -1,20 +1,16 @@
 const nodemailer = require('nodemailer');
 const transporter = nodemailer.createTransport({
-	service: 'Gmail',
-	secure: false, // use SSL
-	port: 25, // port for secure SMTP
+	host: 'smtp.gmail.com',
+	port: 587,
 	auth: {
 		user: 'rakesh@singhamventures.com',
 		pass: 'rajk@72h',
-	},
-	tls: {
-		rejectUnauthorized: false,
 	},
 });
 
 const sendEmail = async (to, subject, text) => {
 	return await transporter.sendMail({
-		from: 'Homesearch18',
+		from: 'rakesh@singhamventures.com',
 		to,
 		subject,
 		text,
