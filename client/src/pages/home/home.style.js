76e2491a63251d766e2a4c9b636bb6d9cad719b8
Rakesh <rakeshchandra.offcl@gmail.com>
2021-07-19 11:34:58 +0530

    Latest Changes

--- client/src/pages/home/home.style.js
+++
@@ -1,8 +0,0 @@
-import { makeStyles } from '@material-ui/core/styles';
-
-const useStyles = makeStyles((theme) => ({
-	bg: {
-		backgroundColor: theme.fontColorThree,
-	},
-}));
-export default useStyles;
